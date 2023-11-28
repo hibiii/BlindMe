b4bab980d87984d2bdaba09f1f97dd1e7531b3ea
hibi <69405847+hibiii@users.noreply.github.com>
2023-11-28 12:11:12 -0300

    initial commit

---
+++ src/main/java/hibi/blind_me/Main.java
@@ -0,0 +1,13 @@
+package hibi.blind_me;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("BlindMe");
+
+    public static void clientInit() {
+        LOGGER.info("Hello, world");
+    }
+}
