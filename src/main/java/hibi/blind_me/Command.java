64364681a9013724c105dcf58ecec0dd06f8ec4a
hibi <69405847+hibiii@users.noreply.github.com>
2023-12-01 22:44:23 -0300

    add ingame command

---
+++ src/main/java/hibi/blind_me/Command.java
@@ -0,0 +1,52 @@
+package hibi.blind_me;
+
+import org.quiltmc.qsl.command.api.EnumArgumentType;
+import org.quiltmc.qsl.command.api.client.ClientCommandManager;
+import org.quiltmc.qsl.command.api.client.QuiltClientCommandSource;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+
+import hibi.blind_me.ConfigEnums.ServerEffect;
+import net.minecraft.command.CommandBuildContext;
+import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
+import net.minecraft.text.Text;
+
+public final class Command {
+    private Command() {};
+
+    public static void registerCallback(CommandDispatcher<QuiltClientCommandSource> dispatcher, CommandBuildContext ctx, RegistrationEnvironment env) {
+        dispatcher.register(ClientCommandManager.literal("blindme")
+            .then(ClientCommandManager.argument("effect", new EnumArgumentType("off", "blindness", "darkness"))
+                .executes(Command::worldSubcommand)
+            )
+        );       
+    }
+
+    private static int worldSubcommand(CommandContext<QuiltClientCommandSource> cmd) {
+        ServerEffect ef = switch(cmd.getArgument("effect", String.class)) {
+            case "off" -> ServerEffect.OFF;
+            case "blindness" -> ServerEffect.BLINDNESS;
+            case "darkness" -> ServerEffect.DARKNESS;
+            default -> throw new IllegalStateException("Unreachable code executed");
+        };
+
+        if (ConfigManager.CONFIG.getEffectForServer(EffectManager.uniqueId) == ef) {
+            cmd.getSource().sendFeedback(Text.translatable(K_EFFECT_ALREADY_SET));
+            return 0;
+        }
+        ConfigManager.CONFIG.servers.getRealValue().put(EffectManager.uniqueId, ef);
+        cmd.getSource().sendFeedback(switch(ef) {
+            case OFF -> Text.translatable(K_EFFECT_OFF);
+            case BLINDNESS -> Text.translatable(K_EFFECT_SET, Text.translatable("effect.minecraft.blindness"));
+            case DARKNESS -> Text.translatable(K_EFFECT_SET, Text.translatable("effect.minecraft.darkness"));
+        });
+        return 0;
+    }
+
+    private static final String
+        K_EFFECT_ALREADY_SET = "blindme.command.effect_already_set",
+        K_EFFECT_SET = "blindme.command.set_effect",
+        K_EFFECT_OFF = "blindme.command.disable_effect"
+    ;
+}
--- src/main/java/hibi/blind_me/Main.java
+++ src/main/java/hibi/blind_me/Main.java
@@ -1,5 +1,6 @@
 package hibi.blind_me;
 
+import org.quiltmc.qsl.command.api.client.ClientCommandRegistrationCallback;
 import org.quiltmc.qsl.lifecycle.api.client.event.ClientWorldTickEvents;
 import org.quiltmc.qsl.networking.api.client.ClientPlayConnectionEvents;
 import org.slf4j.Logger;
@@ -14,5 +15,6 @@ public class Main {
         ClientWorldTickEvents.START.register(EffectManager::tickCallback);
         ClientPlayConnectionEvents.DISCONNECT.register(EffectManager::disconnectCallback);
         ClientPlayConnectionEvents.JOIN.register(EffectManager::joinCallback);
+        ClientCommandRegistrationCallback.EVENT.register(Command::registerCallback);
     }
 }
