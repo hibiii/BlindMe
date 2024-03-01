a92e271da4ac7a944a414f9b4917d77b629ce4d1
hibi <69405847+hibiii@users.noreply.github.com>
2024-03-01 14:19:43 -0300

    add more details to the touch text

--- src/main/java/hibi/blind_me/TouchTextRenderer.java
+++ src/main/java/hibi/blind_me/TouchTextRenderer.java
@@ -12,6 +12,8 @@ public class TouchTextRenderer {
     
     private MinecraftClient client;
     private @Nullable Text text;
+    private @Nullable Text subtitle;
+    private Text distanceText;
     private int ticksDislaying;
     private int fadeoutTicks;
 
@@ -19,11 +21,13 @@ public class TouchTextRenderer {
         this.client = client;
     }
 
-    public void setText(@Nullable Text text) {
+    public void setText(@Nullable Text text, @Nullable Text subtitle, double distance) {
         if (text == null) {
             return;
         }
         this.text = text;
+        this.subtitle = subtitle;
+        this.distanceText = Text.translatable("blindme.touch.distance", String.format("%.1f", distance));
         this.ticksDislaying = 20;
         this.fadeoutTicks = 10;
     }
@@ -35,11 +39,17 @@ public class TouchTextRenderer {
         int scaledWidth = graphics.getScaledWindowWidth();
         int scaledHeight = graphics.getScaledWindowHeight();
         TextRenderer textRenderer = client.textRenderer;
-        int textWidth = textRenderer.getWidth(this.text);
         int alpha = MathHelper.clamp((int)((this.fadeoutTicks - tickDelta) * 25.5f), 0, 255);
         if (alpha > 8) {
             alpha = alpha << 24 & 0xFF000000;
+            int textWidth = textRenderer.getWidth(this.text);
             graphics.drawText(textRenderer, this.text, (scaledWidth - textWidth) / 2, scaledHeight / 2 + 16, 0xFFFFFF | alpha, true);
+            textWidth = textRenderer.getWidth(this.distanceText);
+            graphics.drawText(textRenderer, this.distanceText, (scaledWidth - textWidth) / 2, scaledHeight / 2 + 25, 0xAAAAAA | alpha, true);
+            if (this.subtitle != null) {
+                textWidth = textRenderer.getWidth(this.subtitle);
+                graphics.drawText(textRenderer, this.subtitle, (scaledWidth - textWidth) / 2, scaledHeight / 2 + 34, 0xAAAAAA | alpha, true);
+            }
         }
     }
 
@@ -51,6 +61,7 @@ public class TouchTextRenderer {
                 this.fadeoutTicks -= 1;
             } else {
                 this.text = null;
+                this.subtitle = null;
             }
         }
     }
--- src/main/java/hibi/blind_me/Touching.java
+++ src/main/java/hibi/blind_me/Touching.java
@@ -7,10 +7,12 @@ import com.mojang.blaze3d.platform.InputUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBind;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 
 public final class Touching {
     Touching() {}
@@ -24,12 +26,29 @@ public final class Touching {
         Main.touchTextRenderer.tick();
         if (TOUCH_KEY.wasPressed()) {
             client.player.swingHand(Hand.field_5808);
-            Text text = switch(client.crosshairTarget.getType()) {
-                case field_1333 -> null; // miss
-                case field_1332 -> world.getBlockState(((BlockHitResult)client.crosshairTarget).getBlockPos()).getBlock().getName(); // block
-                case field_1331 -> ((EntityHitResult)client.crosshairTarget).getEntity().getName(); // entity
-            };
-            Main.touchTextRenderer.setText(text);
+            HitResult hit = client.crosshairTarget;
+            // TODO: upgrade to instanceof switch when JAVA 21 hits prod
+            Text text = null, subtitle = null;
+            double distance = client.cameraEntity.getCameraPosVec(0).distanceTo(hit.getPos());
+            System.err.println(hit.getType());
+            switch(hit.getType()) {
+                // Miss
+                case field_1333 -> { return; }
+                // Block
+                case field_1332 -> {
+                    text = world.getBlockState(((BlockHitResult)hit).getBlockPos()).getBlock().getName();
+                }
+                case field_1331 -> {
+                    Entity entity = ((EntityHitResult)hit).getEntity();
+                    text = entity.getType().getName();
+                    if (entity.hasCustomName()) {
+                        subtitle = entity.getCustomName();
+                    }
+                    break;
+                }
+                default -> { throw new IllegalStateException("HitResult is not MISS, BLOCK, or ENTITY"); }
+            }
+            Main.touchTextRenderer.setText(text,  subtitle, distance);
         }
     }
 
