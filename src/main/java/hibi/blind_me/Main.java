be4dfe220374a37220eb24fa8c66ca379d516592
hibi <69405847+hibiii@users.noreply.github.com>
2024-03-04 21:15:23 -0300

    move status effect registration to serverside

--- src/main/java/hibi/blind_me/Main.java
+++ src/main/java/hibi/blind_me/Main.java
@@ -18,8 +18,12 @@ import net.minecraft.util.Identifier;
 public class Main {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("BlindMe");
-    public static final StatusEffect TRULY_BLIND = new TrulyBlindEffect();
     public static TouchTextRenderer touchTextRenderer;
+
+    // Implementation notice: while BlindMe is a client-only mod, it may not be
+    // a good idea to register a status effect on the client init phase, because
+    // the server registry may be frozen by then.
+    public static final StatusEffect TRULY_BLIND = new TrulyBlindEffect();
 
     public static void clientInit() {
         Manager.init();
@@ -30,7 +34,11 @@ public class Main {
         ClientPlayConnectionEvents.JOIN.register(EffectManager::joinCallback);
         ClientCommandRegistrationCallback.EVENT.register(Command::registerCallback);
         KeyBindingHelper.registerKeyBinding(Touching.TOUCH_KEY);
-        // Temporary hack while a better solution is worked out
+    }
+
+    // Since BlindMe is designed to be client-only, it's okay to throw linkage errors on dedi server
+    public static void serverInit() {
+        // FIXME: Vanilla players cannot join LAN worlds hosted by BlindMe clients
         Registry.register(Registries.STATUS_EFFECT, new Identifier("blindme", "truly_blind"), TRULY_BLIND);
     }
 
