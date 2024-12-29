fe0db37ea86ecfb3f66a14797fc4c63ba2b18478
hibi <69405847+hibiii@users.noreply.github.com>
2024-12-29 16:15:28 -0300

    Fix injection enabling sprinting while blind

--- src/main/java/hibi/blind_me/mix/ClientPlayerEntityMixin.java
+++ src/main/java/hibi/blind_me/mix/ClientPlayerEntityMixin.java
@@ -2,33 +2,29 @@ package hibi.blind_me.mix;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import hibi.blind_me.EffectManager;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.registry.Holder;
+import net.minecraft.entity.effect.StatusEffects;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
 
-    @Redirect(
-        method = "canStartSprinting()Z",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasStatusEffect(Lnet/minecraft/registry/Holder;)Z"
-        )
+    @Inject(
+        method = "method_65525()Z",
+        at = @At("HEAD"),
+        cancellable = true
     )
-    boolean sprintIgnoresBlindness(ClientPlayerEntity that, Holder<StatusEffect> effect) {
+    void sprintIgnoresBlindness(CallbackInfoReturnable<Boolean> ci) {
         StatusEffectInstance modEf = EffectManager.getModEffect();
-        StatusEffectInstance playerEf = that.getStatusEffect(effect);
-        if (playerEf == null) {
-            return false;
+        StatusEffectInstance playerEf = ((ClientPlayerEntity)(Object)this).getStatusEffect(StatusEffects.BLINDNESS);
+        if (modEf != null && modEf == playerEf) {
+            if (((StatusEffectInstanceAccessor)modEf).getHiddenEffect() == null) {
+                ci.setReturnValue(false);
+            }
         }
-        if (playerEf == modEf) {
-            return ((StatusEffectInstanceAccessor)modEf).getHiddenEffect() != null;
-        }
-        return true;
     }
 }
