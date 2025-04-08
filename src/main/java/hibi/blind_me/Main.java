d4108b09a253b55afe1eb6496a319ba502675997
hibi <69405847+hibiii@users.noreply.github.com>
2025-04-08 00:20:38 -0300

    Fix #7

--- src/main/java/hibi/blind_me/Main.java
+++ src/main/java/hibi/blind_me/Main.java
@@ -1,7 +1,11 @@
 package hibi.blind_me;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonSyntaxException;
 
 import hibi.blind_me.config.Config;
 import hibi.blind_me.config.ConfigFile;
@@ -14,7 +18,7 @@ public class Main {
     public static final Logger LOGGER = LoggerFactory.getLogger("BlindMe");
     public static Config CONFIG;
 
-    public static void clientInit() {
+    public static void clientInit() throws JsonSyntaxException, IOException {
         CONFIG = ConfigFile.load();
         CONFIG.configureInstance();
         ClientTickEvents.START_CLIENT_TICK.register(EffectManager::tickCallback);
--- src/main/java/hibi/blind_me/config/ConfigFile.java
+++ src/main/java/hibi/blind_me/config/ConfigFile.java
@@ -2,12 +2,15 @@ package hibi.blind_me.config;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 import hibi.blind_me.Main;
 import net.fabricmc.loader.api.FabricLoader;
@@ -19,13 +22,13 @@ public final class ConfigFile {
      * Loads the configuration from disk.
      * When an error occours, this function returns a default configuration instead.
      */
-    public static Config load() {
+    public static Config load() throws IOException, JsonSyntaxException {
         try (var reader = new BufferedReader(new FileReader(PATH))) {
             var json = Files.readString(Path.of(PATH));
             var config = new Gson().fromJson(json, Config.class);
             return config;
-        } catch (Exception e) {
-            Main.LOGGER.error("Could not load config file: ", e);
+        } catch (FileNotFoundException e) {
+            Main.LOGGER.warn("Config file not found, using default configuration.");
             return new Config();
         }
     }
