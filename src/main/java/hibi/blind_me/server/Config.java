3ae463189e105c9407d9444b7e7b73f4bdaf8a31
hibi <69405847+hibiii@users.noreply.github.com>
2025-09-14 13:54:08 -0300

    Make server config file nicer
    Thanks @maddymeows for proofreading

--- src/main/java/hibi/blind_me/server/Config.java
+++ src/main/java/hibi/blind_me/server/Config.java
@@ -6,11 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import java.util.Properties;
 
 import hibi.blind_me.config.ServerEffect;
 import net.fabricmc.loader.api.FabricLoader;
@@ -27,10 +23,31 @@ public class Config {
     public boolean creativeBypass = false;
     public boolean spectatorBypass = true;
 
-    public static Config load() throws IOException, JsonSyntaxException {
+    public static Config load() throws IOException {
         try (var reader = new BufferedReader(new FileReader(PATH))) {
-            var json = Files.readString(Path.of(PATH));
-            var config = new Gson().fromJson(json, Config.class);
+            var props = new Properties();
+            props.load(reader);
+            var config = new Config();
+            config.effect = switch(props.getProperty("effect").toLowerCase()) {
+                case "blindness" -> ServerEffect.BLINDNESS;
+                case "darkness" -> ServerEffect.BLINDNESS;
+                default -> throw new IllegalArgumentException("Bad effect in config file, expected \"blindness\" or \"darkness\"");
+            };
+            config.creativeBypass = switch(props.getProperty("creative_bypass").toLowerCase()) {
+                case "true" -> true;
+                case "false" -> false;
+                default -> throw new IllegalArgumentException("Bad creative bypass in config file, expected \"true\" or \"false\"");
+            };
+            config.spectatorBypass = switch(props.getProperty("spectators_bypass").toLowerCase()) {
+                case "true" -> true;
+                case "false" -> false;
+                default -> throw new IllegalArgumentException("Bad spectator bypass in config file, expected \"true\" or \"false\"");
+            };
+            config.opsBypass = switch(props.getProperty("ops_bypass").toLowerCase()) {
+                case "true" -> true;
+                case "false" -> false;
+                default -> throw new IllegalArgumentException("Bad OPs bypass in config file, expected \"true\" or \"false\"");
+            };
             return config;
         } catch (FileNotFoundException e) {
             Main.LOGGER.warn("Config file not found, creating a new file and using default configuration.");
@@ -42,12 +59,42 @@ public class Config {
 
     private void save() {
         try (var writer = new BufferedWriter(new FileWriter(PATH))) {
-            var json = new Gson().toJson(this);
-            writer.write(json);
+            writer.write(DEFAULT_CONFIG_FILE);
         } catch (Exception e) {
             Main.LOGGER.error("Could not save the config file", e);
         }
     }
 
     private static final String PATH = FabricLoader.getInstance().getConfigDir().toAbsolutePath().resolve("blindme.server.json").toString();
+
+    private static final String DEFAULT_CONFIG_FILE = """
+        # The selected vision-impairing effect for this server.
+        # Can be one of "blindness" or "darkness".
+        effect = blindness
+        # When clients without BlindMe log in, they will be given that effect purely at
+        # the network level, but the real player in the world will not have it. When a
+        # client with BlindMe logs in, it will enforce the chosen effect, since it
+        # employs quality-of-life improvements, such as the ability to sprint when
+        # blinded, and visual strobe reduction.
+
+        # Controls whether players in creative mode have their effect removed.
+        # - Only available for clients using BlindMe. The server-side fallback system
+        #   is relatively primitive.
+        # Can be one of "true" or "false".
+        creative_bypass = false
+
+        # Controls whether players in spectator mode have their effect removed.
+        # - Only available for clients using BlindMe. The server-side fallback system
+        #   is relatively primitive.
+        # Can be one of "true" or "false".
+        spectators_bypass = true
+
+        # Controls whether operators using BlindMe can change their settings.
+        # - Applicable to players with permission level 2 or higher: game masters,
+        #   administrators and owners.
+        # - Only available for clients using BlindMe. The server-side fallback system
+        #   is relatively primitive.
+        # Can be one of "true" or "false".
+        ops_bypass = false
+        """;
 }
--- src/main/java/hibi/blind_me/server/Main.java
+++ src/main/java/hibi/blind_me/server/Main.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonSyntaxException;
 
-import hibi.blind_me.config.ServerEffect;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 
@@ -25,9 +24,6 @@ public class Main {
             return;
         }
         CONFIG = Config.load();
-        if (CONFIG.effect == ServerEffect.OFF || CONFIG.effect == null) {
-            throw new IllegalStateException("Starting a server with phantom effect set to null or OFF is not supported.");
-        }
         Networking.register();
     }
 }
