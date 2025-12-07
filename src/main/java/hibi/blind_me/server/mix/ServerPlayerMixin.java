216f91285e9975274aacdb5177f38500efa2d1e9
hibi <69405847+hibiii@users.noreply.github.com>
2025-12-07 11:41:35 -0300

    Fix ServerPlayerMixin

--- src/main/java/hibi/blind_me/server/mix/ServerPlayerMixin.java
+++ src/main/java/hibi/blind_me/server/mix/ServerPlayerMixin.java
@@ -1,7 +1,6 @@
 package hibi.blind_me.server.mix;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import hibi.blind_me.server.Main;
@@ -15,21 +14,18 @@ import net.minecraft.server.network.ServerGamePacketListenerImpl;
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin {
 
-    @Shadow
-    public ServerGamePacketListenerImpl connection;
-
     @WrapWithCondition(
         method = "onEffectAdded",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;sendPacket(Lnet/minecraft/network/protocol/Packet;)V"
+            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;send(Lnet/minecraft/network/protocol/Packet;)V"
         )
     )
     boolean preventApplication(ServerGamePacketListenerImpl handler, Packet<?> packet) {
         if (!Main.enabled || Networking.acknowledgements.contains(Networking.connectionFromHandler(handler))) {
             return true;
         }
-        var pack = (ClientboundUpdateMobEffectPacket)packet; // Packet will always be EntityStatusEffectS2CPacket
+        var pack = (ClientboundUpdateMobEffectPacket)packet; // Packet will always be ClientboundUpdateMobEffectPacket
         return pack.getEffect() != Main.CONFIG.effect.getType();
     }
 
@@ -37,7 +33,7 @@ public class ServerPlayerMixin {
         method = "onEffectUpdated",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;sendPacket(Lnet/minecraft/network/protocol/Packet;)V"
+            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;send(Lnet/minecraft/network/protocol/Packet;)V"
         )
     )
     // IMPORTANT IMPLEMENTATION DETAIL: Blindness and Darkness effects do not change when upgraded, therefore, we can
@@ -46,7 +42,7 @@ public class ServerPlayerMixin {
         if (!Main.enabled || Networking.acknowledgements.contains(Networking.connectionFromHandler(handler))) {
             return true;
         }
-        var pack = (ClientboundUpdateMobEffectPacket)packet; // Packet will always be EntityStatusEffectS2CPacket
+        var pack = (ClientboundUpdateMobEffectPacket)packet; // Packet will always be ClientboundUpdateMobEffectPacket
         return pack.getEffect() != Main.CONFIG.effect.getType();
     }
 
@@ -54,14 +50,14 @@ public class ServerPlayerMixin {
         method = "onEffectsRemoved",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;sendPacket(Lnet/minecraft/network/protocol/Packet;)V"
+            target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;send(Lnet/minecraft/network/protocol/Packet;)V"
         )
     )
     boolean preventRemoval(ServerGamePacketListenerImpl handler, Packet<?> packet) {
         if (!Main.enabled || Networking.acknowledgements.contains(Networking.connectionFromHandler(handler))) {
             return true;
         }
-        var pack = (ClientboundRemoveMobEffectPacket)packet; // Packet will always be RemoveEntityStatusEffectS2CPacket
+        var pack = (ClientboundRemoveMobEffectPacket)packet; // Packet will always be ClientboundRemoveMobEffectPacket
         return pack.effect() != Main.CONFIG.effect.getType();
     }
 }
