21c441cc214f7a8b5e65197427ccf656307f7199
hibi <69405847+hibiii@users.noreply.github.com>
2026-03-24 18:21:01 -0300

    Update to 26.1

--- src/main/java/hibi/blind_me/mix/LightmapRenderStateExtractorMixin.java
+++ src/main/java/hibi/blind_me/mix/LightmapRenderStateExtractorMixin.java
@@ -3,22 +3,14 @@ package hibi.blind_me.mix;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Slice;
-
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import hibi.blind_me.EffectManager;
 import hibi.blind_me.Main;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.OptionInstance;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LightmapRenderStateExtractor;
-import net.minecraft.core.Holder;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
 
 @Mixin(LightmapRenderStateExtractor.class)
 public class LightmapRenderStateExtractorMixin {
