0b00bc567642473ce26eda8729b696b12ef7e323
hibi <69405847+hibiii@users.noreply.github.com>
2026-05-26 19:03:22 -0300

    Fix mixins

--- src/main/java/hibi/blind_me/mix/GuiMixin.java
+++ src/main/java/hibi/blind_me/mix/GuiMixin.java
@@ -7,9 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import com.llamalad7.mixinextras.sugar.Local;
-
 import hibi.blind_me.Main;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.Gui;
@@ -18,13 +15,10 @@ import net.minecraft.client.gui.screens.Screen;
 @Mixin(Gui.class)
 public class GuiMixin {
     @Inject(
-        method = "buildInitialScreens",
-        at = @At(
-            value = "INVOKE_ASSIGN",
-            target = "Ljava/util/ArrayList;<init>()Ljava/util/ArrayList;"
-        )
+        method = "addInitialScreens",
+        at = @At("TAIL")
     )
-    void addIrisWarningScreen(List<Function<Runnable, Screen>> list, CallbackInfoReturnable<Boolean> info, @Local List<Function<Runnable, Screen>> screens) {
+    void addIrisWarningScreen(List<Function<Runnable, Screen>> list, CallbackInfoReturnable<Boolean> info) {
         if(FabricLoader.getInstance().isModLoaded("iris") && !Main.CONFIG.hasSeenIrisWarning) {
             list.add(Main::produceIrisWarningScreen);
         }
